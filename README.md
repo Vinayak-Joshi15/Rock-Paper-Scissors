9d609874ea9127443a6678ad6d42c66794a0e551
VJ <139743089+Vinayak-Joshi15@users.noreply.github.com>
2024-04-07 10:25:26 +0530

    Made the project

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Rock-Paper-Scissors
+
+Creating a rock-paper-scissors game for odin-project
---
+++ game.js
@@ -0,0 +1,57 @@
+console.log("Lets Play!!");
+
+function start(){
+    let n = prompt("Enter your move: ");
+    n = n.toLowerCase;
+    return n
+}
+
+function computer(){
+    let m = Math.floor(1 + Math.random()*2);
+
+    switch(m){
+        case 1:
+        return "rock";
+        break;
+        case 2:
+        return "paper"
+        break;
+        case 3:
+        return "scissor"
+        break;
+    }
+}
+
+function game(start,computer){
+    if(start == computer){
+        console.log("Its a Draw!!");
+    }
+    else if(start == "rock"){
+        if(computer == "paper"){
+            console.log("Computer won with paper");
+        }
+        else{
+            console.log("You win");
+        }
+    }
+    
+    else if(start == "paper"){
+        if(computer == "scissor"){
+            console.log("Computer won with scissor");
+        }
+        else{
+            console.log("You win with paper");
+        }
+    }
+
+    else if(start == "scissor"){
+        if(computer == "rock"){
+            console.log("Computer won with rock");
+        }
+        else{
+            console.log("You win with scissor");
+        }
+    }
+}
+
+game(start(),computer());
